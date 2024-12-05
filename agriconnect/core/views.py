a90d57759304c5ffcfe3a0da800c3688d8570677
Ivn Nyaga Muchiri <41712ivnnyagaitl6s023g1@gmail.com>
2024-12-05 21:05:31 +0300

    initial commit

---
+++ agriconnect/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('dashboard')  # Redirect to a dashboard
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'core/register.html', {'form': form})
